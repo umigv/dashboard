11c7b5bd61b6bc56f7dca32e5bd451af7e3b7fdf
Ethan Hardy <102838865+978-9-17-637879-3@users.noreply.github.com>
2025-01-26 15:34:46 -0500

    use __dirname for paths

--- src/express.ts
+++ src/express.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 import fs from "fs";
+import path from "path";
 
 export function setupExpress() {
     const app = express();
-    const index_page = fs.readFileSync("static/index.html", "utf-8");
+    const index_page = fs.readFileSync(path.join(__dirname, "../static/index.html"), "utf-8");
     
     app.get("/", (req: Request, res: Response) => {
         res.end(index_page);
