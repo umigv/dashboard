88fb92e143ff54e08be85bed8ef0a4a32e505120
serenasaleh <serenasa@umich.edu>
2025-02-02 16:48:18 -0500

    feat: initial frontend design

--- src/express.ts
+++ src/express.ts
@@ -4,10 +4,11 @@ import path from "path";
 
 export function setupExpress() {
     const app = express();
-    const index_page = fs.readFileSync(path.join(__dirname, "../static/index.html"), "utf-8");
+
+    app.use('/static', express.static('static'));
     
     app.get("/", (req: Request, res: Response) => {
-        res.end(index_page);
+        res.end(fs.readFileSync(path.join(__dirname, "../static/index.html"), "utf-8"));
     });
 
     return app;
