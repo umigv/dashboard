19aafee56cda71fd670ac798b421cd4f75eeab4a
Ethan Hardy <102838865+hardy-ethan@users.noreply.github.com>
2025-02-16 15:59:06 -0500

    feat: greatly improve camera feed performance

--- src/ros.ts
+++ src/ros.ts
@@ -3,6 +3,58 @@ require('dotenv').config();
 import * as rclnodejs from "rclnodejs";
 import { Request, Response, Application } from "express";
 import { IO } from "./types";
+import sharp from "sharp";
+
+function setupCameraEndpoint(app: Application, node: rclnodejs.Node) {
+    const MJPEGBOUNDARY = "--mjpegstream";
+    const JPEG_QUALITY = 80;
+
+    const mjpegRequests = new Set<Response>();
+
+    app.get("/camera/", async (req: Request, res: Response) => {
+
+        res.writeHead(200, {
+            'Cache-Control': 'no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0',
+            'Pragma': 'no-cache',
+            'Connection': 'close',
+            'Content-Type': `multipart/x-mixed-replace; boundary=${MJPEGBOUNDARY}`
+        });
+
+        mjpegRequests.add(res);
+
+        res.on('close', () => mjpegRequests.delete(res));
+    });
+
+    node.createSubscription("sensor_msgs/msg/Image", "camera", async (msgPromise) => {
+        const rosImage = (await msgPromise) as rclnodejs.sensor_msgs.msg.Image;
+
+        const numChannels = Math.floor(rosImage.step / rosImage.width);
+        if(numChannels !== 1 && numChannels !== 3 && numChannels !== 4) {
+            console.error(`Unsupported number of channels: ${numChannels}`);
+            return;
+        }
+
+        // Convert msg/Image -> JPEG -> MJPEG and write to stream endpoint
+        const imageAsJPEG = await sharp(Buffer.from(rosImage.data), {
+            raw: {
+                width: rosImage.width,
+                height: rosImage.height,
+                channels: numChannels
+            }
+        })
+            .toFormat('jpeg', { quality: JPEG_QUALITY })
+            .toBuffer();
+
+        const frame = Buffer.concat([
+            Buffer.from(`\r\n${MJPEGBOUNDARY}\r\n`),
+            Buffer.from('Content-Type: image/jpeg\r\n'),
+            Buffer.from(`Content-Length: ${imageAsJPEG.length}\r\n\r\n`),
+            imageAsJPEG
+        ]);
+
+        mjpegRequests.forEach(res => res.write(frame));
+    });
+}
 
 export function setupROS(app: Application, io: IO) {
     const node = new rclnodejs.Node("umarv_dashboard_node");
@@ -10,17 +62,18 @@ export function setupROS(app: Application, io: IO) {
     
     const modePublisher = node.createPublisher("std_msgs/msg/Int32", "is_auto");
 
-    node.createSubscription("sensor_msgs/msg/Image", "camera", async (msgPromise) => {
-        const imageData = (await msgPromise) as rclnodejs.sensor_msgs.msg.Image;
-        io.emit("camera", JSON.stringify(imageData));
-    });
+    setupCameraEndpoint(app, node);
 
     if (!process.env.PROD) {
         // Publish fake camera data for development
         const cameraPublisher = node.createPublisher("sensor_msgs/msg/Image", "camera");
 
+        const IMAGE_WIDTH = 1280;
+        const IMAGE_HEIGHT = 720;
+        const FRAME_RATE = 10;
+
         setInterval(() => {
-            const imageData = new Uint8Array(640 * 480 * 3);
+            const imageData = new Uint8Array(IMAGE_WIDTH * IMAGE_HEIGHT * 3);
             for (let i = 0; i < imageData.length; i++) {
                 imageData[i] = Math.floor(Math.random() * 256);
             }
@@ -33,14 +86,14 @@ export function setupROS(app: Application, io: IO) {
                     },
                     frame_id: "camera"
                 },
-                height: 480,
-                width: 640,
+                height: IMAGE_HEIGHT,
+                width: IMAGE_WIDTH,
                 encoding: "rgb8",
                 is_bigendian: 0,
-                step: 640 * 3,
+                step: IMAGE_WIDTH * 3,
                 data: imageData
             });
-        }, 1000);
+        }, 1000 / FRAME_RATE);
     }
 
     node.spin(0);
