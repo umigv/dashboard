697ae9a8d2105c29bce3ba704befa6e4c15e2169
Ethan Hardy <102838865+hardy-ethan@users.noreply.github.com>
2025-04-08 20:54:00 -0400

    refactor(backend): helper funtions for mode control and gps update

--- src/ros.ts
+++ src/ros.ts
@@ -9,19 +9,8 @@ import { CameraHandler } from "./camera";
 export function setupROS(app: Application, io: IO) {
     const node = new rclnodejs.Node("umarv_dashboard_node");
     
-    const modePublisher = node.createPublisher("std_msgs/msg/Int32", "is_auto");
-
-    node.createSubscription("sensor_msgs/msg/NavSatFix", "gps_coords", async (msg) => {
-        const gpsData = (await msg) as rclnodejs.sensor_msgs.msg.NavSatFix;
-
-        io.emit("sensorData", {
-            gps: {
-                latitude: gpsData.latitude,
-                longitude: gpsData.longitude,
-                altitude: gpsData.altitude,
-            }
-        });
-    });
+    modeControl(app, node);
+    gpsUpdate(io, node);
 
     const cameraHandler = new CameraHandler();
     cameraHandler.setupCameraEndpoint(app, node);
@@ -30,20 +19,6 @@ export function setupROS(app: Application, io: IO) {
     }
 
     node.spin(0);
-
-    app.post("/changeMode", (req: Request, res: Response) => {
-        const mode: string = req.body.mode;
-
-        if (!["autonomous", "teleop"].includes(mode)) {
-            res.end("Invalid mode!")
-        }
-
-        modePublisher.publish({
-            data: req.body.mode === "autonomous" ? 1 : 0
-        });
-
-        res.end(`Changed mode to ${mode}!`);
-    })
 
     app.post("/spinNode", (req: Request, res: Response) => {
         if (node.spinning) {
@@ -67,4 +42,36 @@ export function setupROS(app: Application, io: IO) {
     });
 
     return node;
+}
+
+function modeControl(app: Application, node: rclnodejs.Node) {
+    const modePublisher = node.createPublisher("std_msgs/msg/Int32", "is_auto");
+
+    app.post("/changeMode", (req: Request, res: Response) => {
+        const mode: string = req.body.mode;
+
+        if (!["autonomous", "teleop"].includes(mode)) {
+            res.end("Invalid mode!")
+        }
+
+        modePublisher.publish({
+            data: req.body.mode === "autonomous" ? 1 : 0
+        });
+
+        res.end(`Changed mode to ${mode}!`);
+    })
+}
+
+function gpsUpdate(io: IO, node: rclnodejs.Node) {
+    node.createSubscription("sensor_msgs/msg/NavSatFix", "gps_coords", async (msg) => {
+        const gpsData = (await msg) as rclnodejs.sensor_msgs.msg.NavSatFix;
+
+        io.emit("sensorData", {
+            gps: {
+                latitude: gpsData.latitude,
+                longitude: gpsData.longitude,
+                altitude: gpsData.altitude,
+            }
+        });
+    });
 }
